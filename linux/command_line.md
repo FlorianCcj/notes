f39596ee11c7e2f4548542f9e427598e3ac5acd1
florianccj <florianccj@gmail.com>
2018-03-31 19:34:01 +0200

    [+][work linux command]

--- linux/command_line.md
+++ linux/command_line.md
@@ -6,14 +6,19 @@ CommandLinePowerUser.com
 install oh my zsh: http://ohmyz.sh
 curl -L http://install.ohmyz.sh | sh
 # if fail: `sudo chsh -s $(which zsh)` or add `zsh` at the last end of ~/.bashrc
-sudo apt-get install moo
-to choose a theme go on github repo of zsh and chack the wiki
 
 subl ~/.bashrc : launch sublime text with this file
 open ~/.bashrc : launch default editor with this file
 
 sometimes some weird character are use, eu need to go to https://github.com/powerline to download which one you need
 
+## moo
+sudo apt-get install moo
+to choose a theme go on github repo of zsh and chack the wiki
 
+## z command
 copy https://raw.githubusercontent.com/rupa/z/master/z.sh in ~/z.sh
 in ~/.zshrc add `. ~/z.sh`
+
+## droit
+changer les sudoers : fichier /etc/sudoers
--- linux/linux.sh
+++ linux/linux.sh
@@ -1,1 +1,82 @@
 sudo passwd USERNAME : set password of a user
+
+# Espace disque
+
+espace disque par partition
+df -h
+espace disque prit par les elements d un dossier
+du -sh /home/user/
+
+# fichier important
+
+/etc/debian_version
+/etc/hosts
+/etc/ssh/sshd_config
+/etc/groups
+/var/log/auth.log
+
+# package
+
+dpkg -i package.deb : install un .deb
+tar cfz file.tgz repertoire : compresse
+tar xfz file.tgz : decompresse
+
+# droit
+
+config permit RootLogin yes
+allowUsers moi toi
+addgroup ssh-allowed
+usermod -a -G ssh-allowed florian
+groups "utilisateur"
+allowUsers denyUsers
+allowGroups denyGroups
+
+
+# debug
+
+sudo service $(cat /etc/X11/default-display-manager | awk '{print substr($1,11)}') restart : lancer le GUI a partir de tty
+echo $PATH
+echo $env
+while true; do date; sleep 5 ; done
+
+reinstall packet precedement delete
+awk '$3 =="remove" {print $1,$2,$4}' /var/log/dpkg.log | tee list 
+sudo apt-get --simulate install $(awk '{print $3}' list mod)
+
+# Reseau
+
+scp /home/user/data/file user@192.168.2.2:/tmp : deposer en scp quelque chose 
+  -r
+  -p 8080 : changement de port 
+
+ss -lmtp | grep 22 : qui ecoute le port 22
+
+ssh user@server
+sshfs user@ip:chemin_sur_la_vm chemin_en_local
+umount chemin_en_local
+
+netstat -ntpl : affiche les ports utilisé
+
+a2enmod
+a2ensite
+a2dismod
+a2dissite
+
+python -m json.tool
+ip -4 addr
+
+# Recherche
+
+grep -lR "coincoin" . : donne la liste des fichiers
+find . | xargs grep "coincoin" -sl : affiche le fichier et la ligne
+find ./ -regex '\./[^.].+' -mtime -4 : fichier modifier il y a moins de 4 jours
+find ./ -regex '\./[^.].+' -amin $((($(date --date='2013-12-28 16:45:00' +%0) - $(date --date='now' +%0)) (60)))
+find -mtime -15
+
+w : qui est connecté
+
+# Bdd
+
+mysqldump -u $db_user -p$db_pwd $db_name > file.sql
+mysql -u $db_user -p$db_pwd $db_name < file.sql
+  -h : permet d ajouterun remote host si c est pas en local 
