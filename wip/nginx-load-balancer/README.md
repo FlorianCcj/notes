94773a131da02448ceeb693eda11b99abc3b6a6c
florianccj <florianccj@gmail.com>
2019-02-10 22:09:37 +0100

    [nginx] test load balancer

---
+++ wip/nginx-load-balancer/README.md
@@ -0,0 +1,7 @@
+source: https://codeburst.io/scaling-out-with-docker-and-nginx-8eda9fb1654c
+
+term:
+docker-compose up
+
+browser:
+localhost:8080
---
+++ wip/nginx-load-balancer/backend/src/index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) =>  {      
+   console.log('console: I just received a GET request on port 3000!');
+   res.send('res: Hello World!');
+});
+
+app.listen(3000, () => {
+   console.log('console: I just connected on port 3000!');
+});
