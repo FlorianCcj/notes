ab8e2eec7a3ed8c923769e5296c361f66590d876
florianccj <florianccj@gmail.com>
2020-03-25 18:00:34 +0100

    [*][doc] docker, remove build later

--- 000-docs/scripts/docker_build_sphinx.sh
+++ 000-docs/scripts/docker_build_sphinx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -rf build
 docker build -t sphinx-build -f docker/Dockerfile-sphinx-builder .
 container_name="$(date +"%s")"
 container_id=$(docker run --name="sphinx-build-${container_name}" --rm -it -d -e USER_ID=$UID sphinx-build bash)
+rm -rf build
 docker cp ${container_id}:/doc/build $(pwd)
 docker stop $(docker ps -aq -f name=sphinx-build-${container_name})
 docker rmi sphinx-build
