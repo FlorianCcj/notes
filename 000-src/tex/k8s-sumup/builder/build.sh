5ca275558502f51216d66af43b957cb3655bda98
florianccj <florianccj@gmail.com>
2022-04-30 21:06:25 +0200

    docs(htcpcp): add tilt and shell docs

--- 000-src/tex/k8s-sumup/builder/build.sh
+++ 000-src/tex/k8s-sumup/builder/build.sh
@@ -5,5 +5,5 @@ docker build -t tex-build -f builder/Dockerfile-tex-builder .
 container_name="$(date +"%s")"
 container_id=$(docker run --name="k8s-sumup-${container_name}" --rm -it -d -e USER_ID=$UID tex-build bash)
 docker cp ${container_id}:/workdir/build $(pwd)
-docker stop $(docker ps -aq -f name=k8s-sumup-${container_name})
+docker rm -f $(docker ps -aq -f name=k8s-sumup-${container_name})
 # docker rmi tex-build
